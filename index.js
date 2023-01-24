5f886eeda7f9a82965ffa4592a1858ccda78d67f
sandeep kumar <sandeep.kumar@logipe.com>
2023-01-24 13:26:30 +0530

    working with puppeteer

--- index.js
+++ index.js
@@ -6,26 +6,24 @@ const controller = require("./router/getTitle")
 const router = express.Router();
 const dotenv = require("dotenv").config()
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-// app.use('/',router);
+
 app.use(controller);
 
-// const dbConnect = require("./dbConnect");
 
-// app.listen(PORT, () => {
-//     console.log(`We are flying on Port ${PORT}`);
-// });
-// const scheduler = require("./job/scrap");
-// scheduler();
 mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true })
 .then(()=>{
     app.listen(PORT, () => {
-        console.log(`We are flying on Port${PORT}`);
+        console.log(`We are flying on Port ${PORT}`);
     })
-    require("./job/scrap")
+    // job scheduler
+    setInterval(() => {
+        require("./job/scrap")
+    }, 10000);
+    
 })
